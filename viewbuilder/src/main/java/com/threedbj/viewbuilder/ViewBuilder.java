c26e39d727cccdb3c5b69eb28cb59050a5a62a9b
Sam Pullman <sampullman@gmail.com>
2019-01-12 00:03:03 -0800

    Gradle update and helper for dividers

--- viewbuilder/src/main/java/com/threedbj/viewbuilder/ViewBuilder.java
+++ viewbuilder/src/main/java/com/threedbj/viewbuilder/ViewBuilder.java
@@ -2,10 +2,34 @@ package com.threedbj.viewbuilder;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.ColorRes;
 
 import com.threedbj.viewbuilder.generic.GenericViewBuilder;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
 public class ViewBuilder extends GenericViewBuilder<ViewBuilder, View> {
+
+    public static void HORIZONTAL_DIVIDER(LinearLayout parent, @ColorRes int color) {
+        new ViewBuilder()
+            .inLinear()
+            .width(MATCH_PARENT)
+            .height(1)
+            .backgroundColor(color)
+            .build(parent);
+    }
+
+    public static void VERTICAL_DIVIDER(LinearLayout parent, @ColorRes int color) {
+        new ViewBuilder()
+            .inLinear()
+            .width(1)
+            .weight(0)
+            .height(MATCH_PARENT)
+            .backgroundColor(color)
+            .build(parent);
+    }
 
     public View build(Context c) {
         return build(c, new View(c));
